7c61c8b79a81a3f5a829ac072ce58f9b6a1a8c89
Sahil P Gaonkar <60326413+SahilGaonkar1099@users.noreply.github.com>
2020-07-07 18:40:06 +0530

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -1,87 +1,63 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-import wordcloud
-import numpy as np
-from matplotlib import pyplot as plt
-from IPython.display import display
-import fileupload
-import io
-import sys
+#Generating Word cloud of words in a input file
+import wordcloud# this is most required library for this project
+from matplotlib import pyplot as plt#to get our final output on screen we make use of this
 
 
-# In[1]:
+def File(): #This function returns file contents as a string of words in lower case.
+    Need=input("Enter the file\n")#input the file name
+    with open(Need,'r') as F:
+        Read=F.read()
+    return Read.lower()
 
 
-def _upload():
-
-    _upload_widget = fileupload.FileUploadWidget()
-
-    def _cb(change):
-        global file_contents
-        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
-        filename = change['owner'].filename
-        print('Uploaded `{}` ({:.2f} kB)'.format(filename, len(decoded.read()) / 2 **10))
-        file_contents = decoded.getvalue()
-
-    _upload_widget.observe(_cb, names='data')
-    display(_upload_widget)
-
-_upload()
-
-
-# In[ ]:
-
-
-def calculate_frequencies(file_contents):
+def calculate_frequencies():
     # Here is a list of punctuations and uninteresting words you can use to process your text
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",     "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being",     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
-    
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", "we",
+                           "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
+                           "they", "them", "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was",
+                           "were", "be", "been", "being", "have", "has", "had", "do", "does", "did", "but", "at", "by",
+                           "with", "from", "here", "when", "where", "how", "all", "any", "both", "each", "few", "more",
+                           "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+
     # LEARNER CODE START HERE
-    file=file_contents.lower()
-    innitial_list_words=file.split()
-    final_list_words=[];S1=""
+    file = File()
+    innitial_list_words = file.split()# to string is converted into list of words 
+    final_list_words = []
+    S1 = ""
     for words in innitial_list_words:
-        help_list=[]
+        help_list = []
         for letter in words.strip():
             if letter.isalpha() and letter not in punctuations:
                 help_list.append(letter)
             else:
                 continue
-        S1="".join(help_list)
+        S1 = "".join(help_list)
 
         final_list_words.append(str(S1))
     for i in final_list_words:
         if i.strip() in uninteresting_words:
             final_list_words.remove(i)
-    result={}
-    text=" ".join(final_list_words)
+    result = {}
+    text = " ".join(final_list_words)
     for L in final_list_words:
         if not L in result:
-            result[L]=0
-        result[L]+=1
-    #wordcloud
+            result[L] = 0
+        result[L] += 1
+    # wordcloud
     cloud = wordcloud.WordCloud()
-    cloud.generate_from_frequencies(result)
+    cloud.generate_from_frequencies(result)#based on the frequencies of words in a list we generate the word cloud
     return cloud.to_array()
 
-
-# In[ ]:
 
 
 # Display your wordcloud image
 
-myimage = calculate_frequencies(file_contents)
-plt.imshow(myimage, interpolation = 'nearest')
+myimage = calculate_frequencies()
+plt.imshow(myimage, interpolation='nearest')
 plt.axis('off')
 plt.show()
 
-
-# In[ ]:
 
 
 
